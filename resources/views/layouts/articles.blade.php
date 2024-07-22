7663b10d08c7feff0568ed8b3479dacf22aa919e
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-07-22 13:58:27 +0200

    code restructuré

--- resources/views/layouts/articles.blade.php
+++ resources/views/layouts/articles.blade.php
@@ -7,14 +7,9 @@
 @section('contenu')
     <h2>Articles</h2>
 
-   @if ($articles)
-       @foreach ($articles as $article)
-        <article>
-            <h2>{{ $article["title"] }}</h2>
-            <p>{{ $article["body"] }}</p>
-        </article>
-        @endforeach
-    @else
-        <p>Oop ! 😭 Aucun article trouvé.</p>
-    @endif
+    @forelse ($articles as $article)
+        @include('articles.index')
+    @empty
+        @include('articles.no-articles')
+    @endforelse
 @endsection
