85331935a3d4a1977b2cf9c3b5e0bbe98046b3e7
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-07-22 11:56:55 +0200

    page about créée

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('layouts.master');
-});
+// Route::get('/', [PagesController::class, 'index']);
+// Route::get('/contact-us', [PagesController::class, 'contact']);
 
-Route::get('/contact-us', function() {
-    return view('layouts.contact');
+// Route::get('/contact-us', function() {
+//     return view('layouts.contact');
+// });
+Route::controller(PagesController::class)
+    ->group(function() {
+        Route::get('/', 'index');
+        Route::get('/contact-us', 'contact');
+        Route::get('/about', 'about');
 });
