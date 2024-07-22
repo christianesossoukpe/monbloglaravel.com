12ca851d6baae81bc15d3ed1ad5ba0d87b4d11a4
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-07-22 11:29:35 +0200

    @yield @extends @section

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -1,1 +1,6 @@
+@extends('layouts.master')
+
+
+@section('contenu')
 <h2>Contactez-nous</h2>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mon Blog Laravel</title>
+</head>
+
 <body>
     <h1>Mon Blog Laravel</h1>
 
     <p>
         <a href="/contact-us">Contactez-nous</a>
     </p>
+
+    <!-- Contenu de toutes les pages ici -->
+    @yield('contenu')
+    <!-- Contenu de toutes les pages ici -->
 </body>
+
+</html>
